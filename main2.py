2ace6f06cf1948998eb756133b210ec5f737f3f0
elciak82 <elciak82@gmail.com>
2017-11-26 15:07:33 +0100

    Python

--- main2.py
+++ main2.py
@@ -3,34 +3,10 @@ def wypisz_samochod(samochod, pojemnosc_silnika):
 
 def czy_samochod(pojemnosc_silnika):
     MAX = 2.5
-    if pojemnosc_silnika > MAX:
+    if pojemnosc_silnika < MAX:
         return False
     else:
         return True
-
-imie = 'Ewelina'
-nazwisko = 'W'
-#komentarz
-pelne_imie = imie + ' ' + nazwisko
-print (pelne_imie)
-
-ilosc_zwierzat_domowych = 2
-cena = 2 + 100
-print (cena)
-
-if __name__ == "__main_imie__":
-    imie = "Ewelina"
-    nazwisko = "Walczak"
-    print ("{0} {1}" .format(imie, nazwisko))
-
-if __name__ == "__main_wiek__":
-    imie = "Ewelina"
-    wiek = 28.5
-    print ("{0} {1}" .format(imie, wiek))
-    if wiek > 63:
-        print ("Nie emeryt")
-    else:
-        print ("Do pracy rodacy")
 
 if __name__ == "__main__":
     samochod = "Opel"
